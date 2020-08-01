69053ea43868d8e88d71486a8b6d4f24f96e8044
Michal Tomczyk <michaltomczyk28@gmail.com>
2020-08-01 18:26:04 +0200

    added dropdown overlaying other content, fetching image and displaying

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+let breeds;
+let input = document.getElementById('input');
+let form = document.getElementById('dog-image-form');
+let resultImage = document.getElementById('result-image');
 
 
 function drawListItems(items) {
@@ -8,31 +12,51 @@ function drawListItems(items) {
 
     document.querySelector('#result-search').innerHTML = output;
     let listItems = [].slice.call(document.querySelector('ul').children);
-    console.log("in func");
+
     listItems.forEach(item => item.addEventListener('click', fillInputWithValue));
 }
 
 function fillInputWithValue(e) {
     let value = e.target.firstChild.data
-    console.log(value);
+
     input.value = value;
     drawListItems([]);
 }
 
+function handleSubmit(e) {
+    e.preventDefault();
+    if (input.value) {
+        fetch(`https://dog.ceo/api/breed/${input.value}/images/random`)
+            .then(res => res.json())
+            .then(value => {
+                let url = value.message;
+                // url = url.slice(0, -1);
+                console.log(url)
+                let output = `<img style="" src="${url}"/>`
+                console.log(output);
+                resultImage.innerHTML = output;
 
-let breeds;
+            });
+    }
+
+}
+
+
+
 fetch('https://dog.ceo/api/breeds/list/all').
     then(res => res.json()).
     then(breedsList => {
         breedsList = breedsList.message;
         breeds = Object.keys(breedsList);
-        let input = document.getElementById('input');
+
         input.addEventListener('input', handleInput);
+        form.addEventListener('submit', handleSubmit);
 
         function handleInput(e) {
             const inputValue = e.target.value;
             const filteredBreeds = inputValue ? breeds.filter(breedName => breedName.includes(inputValue)).slice(0, 5) : [];
-
+            if (!inputValue)
+                resultImage.innerHTML = "";
             drawListItems(filteredBreeds);
         }
 
